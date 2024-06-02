1205157d43f76a5704394d27ab34d0ab25b367cb
sankar prasad biswas <sankar.biswas@geotechinfo.net>
2024-06-02 23:39:02 +0530

    example middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestMiddleware, NestModule } from '@nestjs/common';
 import { UsersService } from './services/users/users.service';
 import { UsersController } from './controllers/users/users.controller';
+import { ExampleMiddleware } from './middlewares/example/example.middleware';
 
 @Module({
   controllers: [ UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+
+export class UsersModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ExampleMiddleware).forRoutes('users')
+  }
+}
